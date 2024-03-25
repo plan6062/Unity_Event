235a7a60749bdd02238014fb0d8d4155fd0d0ced
plan6062 <qwa7854@naver.com>
2024-03-26 00:12:32 +0900

    feat : 유니티 이벤트 구현
    기존 이벤트를 사용하지 않은 코드를 이벤트 사용으로 변경

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public UIManager uiManager;
+    // public UIManager uiManager;
+    // public AchivementSystem achivementSystem;
+    // public GameManager gameManager;
+    // 이벤트 시스템을 이용하지 않은 방법 -> 깔끔하지 못함, 스파게티 코드 발생 우려
 
-    public AchivementSystem achivementSystem;
-
-    public GameManager gameManager;
+    public UnityEvent onPlayerDead;
 
     private void Dead(){
-        uiManager.OnPlayerDead();
-        achivementSystem.UnLockAchivement("Newton's laws of motion");
-        gameManager.OnPlayerDead();
-
+        // uiManager.OnPlayerDead();
+        // achivementSystem.UnLockAchivement("Newton's laws of motion");
+        // gameManager.OnPlayerDead();
+        onPlayerDead.Invoke();
         Debug.Log("죽었다!");
         Destroy(gameObject);
     }
